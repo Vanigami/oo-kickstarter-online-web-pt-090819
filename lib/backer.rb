077d71552eed3e410c24fc8969bf07a52d57bd15
Vanna Winland <vgwinland@gmail.com>
2019-10-09 21:32:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,12 @@
 class Backer 
   attr_reader :backed_projects
   
-def initialize
-  @backed_projects = [] 
+  def initialize
+    @backed_projects = [] 
+  end
+  
+  def back_project()
+    backed_projects << self.back_project
+  end
   
 end
